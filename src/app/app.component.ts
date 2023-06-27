80f9a1f386d632ceaf4ec23aa2680d371b631f89
Vicente Balbontín Jara <vicente.balbontin@sansano.usm.cl>
2023-06-27 16:46:29 -0400

    nuevo diseño aplicado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,4 @@ import { Component, NgModule } from '@angular/core';
 })
 export class AppComponent {
   title = 'alumnos-usm';
-
-  categoria = "Plataformas Estudiantiles"
-
-  cambiar(input: number){
-    if (input == 1){
-      this.categoria = "Plataformas Estudiantiles"
-    }
-    if (input == 2){
-      this.categoria = "Recursos Campus"
-    }
-    if (input == 3){
-      this.categoria = "Beneficios Estudiantiles"
-    }
-    if (input == 4){
-      this.categoria = "Programas de Intercambio"
-    }
-    if (input == 5){
-      this.categoria = "Calendario Académico"
-    }
-    if (input == 6){
-      this.categoria = "Reglamentos y Protocolos"
-    }
-    if (input == 7){
-      this.categoria = "Contacto"
-    }
-  }
 }
